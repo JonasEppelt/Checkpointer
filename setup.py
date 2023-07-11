9ac695253c23058e7fb3981fb60568e79fa35bf1
Jonas Eppelt <jonas.eppelt@gmail.com>
2023-07-11 10:01:23 +0200

    make a package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="checkpointer",
+    version="0.0.1",
+    packages=find_packages(),
+    license="",
+    author="Jonas Eppelt",
+    description="Checkpointer for use in different HPC contexts",
+
+)
