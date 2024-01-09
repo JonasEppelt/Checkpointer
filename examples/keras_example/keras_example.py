83ab5e72eb2d1a175c1cf1ddd3f9b708899c9ac9
Jonas Eppelt <jonas.eppelt@gmail.com>
2024-01-09 18:08:58 +0100

    renamed and commented keras example

--- examples/keras_example/keras_example.py
+++ examples/keras_example/keras_example.py
@@ -1,15 +1,16 @@
 
-# %%
+# imports
 import numpy as np
 import os
-os.environ["CUDA_VISIBLE_DEVICES"]="1"
 from pathlib import Path
 
+# setting the device, kears can train on
+os.environ["CUDA_VISIBLE_DEVICES"]="1"
 import keras
-from checkpointer.keras_callback import KerasCheckpointerCallback
-from keras.callbacks import BackupAndRestore
 
-# %%
+# import the callback for Keras
+from checkpointer.keras_callback import KerasCheckpointerCallback
+
 # Load the data and split it between train and test sets
 (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
 
@@ -25,11 +26,11 @@ print(x_train.shape[0], "train samples")
 print(x_test.shape[0], "test samples")
 
 
-# %%
 # Model parameters
 num_classes = 10
 input_shape = (28, 28, 1)
 
+# create the model
 model = keras.Sequential(
     [
         keras.layers.Input(shape=input_shape),
@@ -40,8 +41,7 @@ model = keras.Sequential(
     ]
 )
 
-
-# %%
+# compile the model
 model.compile(
     loss=keras.losses.SparseCategoricalCrossentropy(),
     optimizer=keras.optimizers.Adam(learning_rate=1e-3),
@@ -50,21 +50,23 @@ model.compile(
     ],
 )
 
-
-# %%
-batch_size = 128
-epochs = 20
+#defince the callbacks 
 callbacks = [
-    KerasCheckpointerCallback(
-        local_checkpoint_file="/work/jeppelt/checkpointing/checkpointer/checkpoint", 
-        checkpoint_every=1, 
-        checkpoint_transfer_mode="shared",
-        checkpoint_transfer_target=Path("/ceph/jeppelt/checkpoint.ckpt")
+    KerasCheckpointerCallback( # setting up the checkpointer callback
+        local_checkpoint_file="/work/jeppelt/checkpointing/checkpointer/checkpoint", # local checkpoint file
+        checkpoint_every=1, # checkpointing every epoch
+        checkpoint_transfer_mode="shared", # using a shared filesystem to move hte checkpoint to a persisten storage
+        checkpoint_transfer_target=Path("/ceph/jeppelt/checkpoint.ckpt") # the target path on the shared filesystem
     ),
     keras.callbacks.EarlyStopping(monitor="val_loss", patience=2),
 ]
 
-# %%
+
+# run the training 
+# It will now automatically check if a checkpoint exists, load it and continue training from it.
+# Test it, by executing this program and interupting it after the first epoch. Then execute it again.
+batch_size = 128
+epochs = 20
 
 model.fit(
     x_train,
@@ -75,12 +77,6 @@ model.fit(
     callbacks=callbacks,
 )
 score = model.evaluate(x_test, y_test, verbose=0)
-
-
-# %%
-
-
-# %%
 
 
 
