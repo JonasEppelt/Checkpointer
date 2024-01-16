824065d87868f860b48893e780a88853bbd9a90f
Jonas Eppelt <jonas.eppelt@gmail.com>
2024-01-16 17:08:06 +0100

    testing with older keras version

--- examples/keras_example/keras_example.py
+++ examples/keras_example/keras_example.py
@@ -7,12 +7,12 @@ from pathlib import Path
 # setting the device, kears can train on
 os.environ["CUDA_VISIBLE_DEVICES"]="1"
 import keras
-
+import tensorflow as tf
 # import the callback for Keras
 from checkpointer.keras_callback import KerasCheckpointerCallback
 
 # Load the data and split it between train and test sets
-(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
 
 # Scale images to the [0, 1] range
 x_train = x_train.astype("float32") / 255
@@ -34,7 +34,7 @@ input_shape = (28, 28, 1)
 model = keras.Sequential(
     [
         keras.layers.Input(shape=input_shape),
-        keras.layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
+        keras.layers.Conv2D(1e2, kernel_size=(3, 3), activation="relu"),
         keras.layers.GlobalAveragePooling2D(),
         keras.layers.Dropout(0.5),
         keras.layers.Dense(num_classes, activation="softmax"),
@@ -55,8 +55,9 @@ callbacks = [
     KerasCheckpointerCallback( # setting up the checkpointer callback
         local_checkpoint_file="/work/jeppelt/checkpointing/checkpointer/checkpoint", # local checkpoint file
         checkpoint_every=1, # checkpointing every epoch
-        checkpoint_transfer_mode="shared", # using a shared filesystem to move hte checkpoint to a persisten storage
-        checkpoint_transfer_target=Path("/ceph/jeppelt/checkpoint.ckpt") # the target path on the shared filesystem
+        checkpoint_transfer_mode="xrootd", # using a shared filesystem to move hte checkpoint to a persisten storage
+        checkpoint_transfer_target="/pnfs/gridka.de/belle/disk-only/LOCAL/user/jeppelt/keras_checkpoint7.test", # the target path on the shared filesystem
+        xrootd_server_name="root://dcachexrootd-kit.gridka.de:1094",
     ),
     keras.callbacks.EarlyStopping(monitor="val_loss", patience=2),
 ]
